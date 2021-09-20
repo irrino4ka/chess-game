ac43b2bb5757f0069051349e1e8309c5249ff329
Iryna Ielizarova <iryna.ielizarova@eleks.com>
2021-09-20 10:17:45 +0300

    Auto-generated commit [ci skip]

--- src/app/feature/piece/piece.ts
+++
@@ -1,9 +0,0 @@
-import { PositionCoord, PieceColor } from '../../core/model';
-
-export interface Piece {
-  color: PieceColor;
-  unicode: string;
-
-  possibleMoves(currentPosition: PositionCoord): Array<PositionCoord>;
-
-}
